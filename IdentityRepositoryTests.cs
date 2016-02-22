632033cef41fdee4a7fddfd4b2e5415ee98b6fbf
Sean Craswell <scraswell@gmail.com>
2016-02-21 21:09:58 -0400

    Initial commit

---
+++ IdentityRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+
+using Craswell.DataAccess;
+using Craswell.Models.IdentityManagement;
+
+namespace Craswell.Encryption.UnitTests
+{
+    [TestFixture()]
+    public class IdentityRepositoryTests
+    {
+        [Test()]
+        public void IdentityRepositoryTests_CanPersistIdentity()
+        {
+            var connectionString = @"Data Source=.\IdentityRepositoryTests.db;Version=3;";
+
+            var persistenceLayer = new PersistenceLayer(
+                connectionString,
+                DatabaseType.SQLite);
+
+            var identityRepository = new IdentityRepository(
+                "test passphrase.",
+                persistenceLayer);
+
+            ////var identity = new Identity()
+            ////{
+            ////    AssociatedResource = "http://testsite.com/testsite/login",
+            ////    Description = "Test identity",
+            ////    Notes = "Test notes",
+            ////    Password = "Test password",
+            ////    Title = "Test title",
+            ////    Username = "Test username"
+            ////};
+
+            ////identityRepository.Create(identity);
+
+            var identity = identityRepository.Read(1);
+
+        }
+    }
+}
+
